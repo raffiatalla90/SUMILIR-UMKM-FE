a54166389e0605f0c3c597d0867d7ee251a24114
raffiatalla90 <raffiatalla9090@gmail.com>
2025-12-16 14:00:21 +0700

    fix: Make PWA registration conditional on production mode
    - Only import virtual:pwa-register in production
    - Remove PWA from development to prevent dependency errors
    - Fixes 'virtual:pwa-register' not found error in dev mode

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -67,7 +67,7 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-14ef65e8'], (function (workbox) { 'use strict';
+define(['./workbox-d8aa5131'], (function (workbox) { 'use strict';
 
   self.skipWaiting();
   workbox.clientsClaim();
@@ -79,7 +79,7 @@ define(['./workbox-14ef65e8'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.d4k2jqfqj88"
+    "revision": "0.u3h8jitvhco"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
