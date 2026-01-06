13dd6971480048707dea399e54f9a8f2904b980f
Satria Kusuma <satriaksm.20@gmail.com>
2026-01-06 12:30:19 +0700

    feat: toko-kuliner

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-14ef65e8'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "/index.html",
-    "revision": "0.fgs66bc85io"
+    "revision": "0.ammqqksb4e4"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("/index.html"), {
