ea587ec83dda65746b676c0556fae12c211fe55a
raffiatalla90 <raffiatalla9090@gmail.com>
2026-03-12 20:52:38 +0700

    Disable PWA dev options in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,7 @@ export default defineConfig(({ mode }) => {
       tailwindcss(),
       VitePWA({
         registerType: "autoUpdate",
-        devOptions: { enabled: true },
+        devOptions: { enabled: false },
         manifest: {
           name: "Sumilir",
           short_name: "Sumilir",
